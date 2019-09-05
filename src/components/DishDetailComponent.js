c5b43fcf02a20426da0ceb16796b7f9389fab089
AhmedIbrahimHamed <aih1175@gmail.com>
2019-09-05 16:40:55 +0200

    Functional Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,60 +1,59 @@
 import React, { Component } from 'react'
-import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle
+} from 'reactstrap'
 
-export class DishDetail  extends Component {
 
-    renderComments(dish) {
-        const comments = dish.comments;
-        if(comments){
-            let commentList = comments.map(comObj => {
-                var date = new Date(comObj.date).toDateString();
-                return (
-                    <ul key={comObj.id} className="list-unstyled">
-                        <li>{comObj.comment}</li>
-                        <li>-- {comObj.author} ,{date}</li>
-                    </ul>)
-            });
-            return  (
-            <div className="col-12 col-md-5 m-1">
-               <h5>Comments</h5>
-               {commentList} 
-            </div>)
-        }else{
-            return <div></div>
-        }
-        
-    }
-
-    renderDish(){
-        const dish = this.props.dish;
-        if(dish){
-            return(
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={dish.image}></CardImg>
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    {this.renderComments(dish)}
-                </div>
-            )
-        }else{
-            return <div></div>
-        }
-    }
-    
-    render() {
+function renderComments(dish) {
+    const comments = dish.comments;
+    if (comments) {
+        let commentList = comments.map(comObj => {
+            var date = new Date(comObj.date).toDateString();
+            return (
+                <ul key={comObj.id} className="list-unstyled">
+                    <li>{comObj.comment}</li>
+                    <li>-- {comObj.author} ,{date}</li>
+                </ul>)
+        });
         return (
-            <React.Fragment>
-                {this.renderDish()}
-            </React.Fragment>
-        )
+            <div className="col-12 col-md-5 m-1">
+                <h5>Comments</h5>
+                {commentList}
+            </div>)
+    } else {
+        return <div></div>
     }
+
+}
+
+function renderDish(dish){
+    if (dish) {
+        return (
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image}></CardImg>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                {renderComments(dish)}
+            </div>
+        )
+    } else {
+        return <div></div>
+    }
+}
+
+const DishDetail = ({dish}) => {
+    return (
+        <div>
+            {renderDish(dish)}
+        </div>
+    )
 }
 
 export default DishDetail 
