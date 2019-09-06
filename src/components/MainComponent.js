bd074bb2d1b9fc3d1d80b77011d022d4e0fffe30
AhmedIbrahimHamed <aih1175@gmail.com>
2019-09-06 14:58:30 +0200

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,10 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
 
 
 class Main extends Component {
@@ -13,6 +16,8 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
             // selectedDish: null
         };
     }
@@ -27,8 +32,12 @@ class Main extends Component {
             <div>
                 <Header />
                 <Switch>
-                    <Route path='/home' component={Home} />
+                    <Route path='/home' component={() => <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]} />} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/contactus' component={Contact}/>
                     <Redirect to="/home" />
                 </Switch>
 
