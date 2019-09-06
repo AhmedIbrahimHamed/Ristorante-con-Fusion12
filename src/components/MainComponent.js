d55ce6821e01bdab1e317e3d115d64dbbf8b97d3
AhmedIbrahimHamed <aih1175@gmail.com>
2019-09-06 17:28:26 +0200

    React Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import {PROMOTIONS} from '../shared/promotions';
 import {COMMENTS} from '../shared/comments';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutUsComponent';
 
 
 class Main extends Component {
@@ -25,15 +26,18 @@ class Main extends Component {
     }
     
     render() {
-
-        console.log(this.state.dishes);
-
         const DishWithId = ({match}) => {
             return(
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
                   comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
             );
           };
+
+        const AboutUsWithLeaders = () => {
+            return (
+                <About leaders={this.state.leaders}/>
+            );
+        }
 
         return (
             <div>
@@ -44,6 +48,7 @@ class Main extends Component {
                     dish={this.state.dishes.filter((dish) => dish.featured)[0]}
                     promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
                     leader={this.state.leaders.filter((leader) => leader.featured)[0]} />} />
+                    <Route exact path='/aboutus' component={AboutUsWithLeaders}/>
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact}/>
